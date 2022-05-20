76521044095954bc4dd9b6ecbaa1eeddb3088a8a
Juan Ignacio Caballero <juan.ci.caballero@gmail.com>
2022-05-20 13:24:28 -0300

    Adding test for check methods.

--- src/test/java/com/eda/quoridoreda/CheckingTest.java
+++ src/test/java/com/eda/quoridoreda/CheckingTest.java
@@ -5,24 +5,50 @@ import org.junit.Test;
 
 public class CheckingTest {
 
-    char[] board = null;
-    char[][] normalized = new char[17][17];
+    private char[] board = null;
+    private char[][] normalized = new char[17][17];
     
     @Test
     public void shouldReturnFalseCheckMoveForward() {
-        board = "    N         N                           N                                                                                                                          -*-              S                                                                                             S   S        ".toCharArray();
+        board = "    N         N                                                                                                                                       N              -*-              S                                                                                             S   S        ".toCharArray();
         normalized = Board.armarTablero(board);
-        int[] currentPosition = new int[]{10, 12};
+        int[] currentPositionS = new int[]{10, 12};
+        int[] currentPositionN = new int[]{8, 14};
         
-        assertFalse(Checking.checkMoveForward(normalized, currentPosition, 'S'));
+        assertFalse(Checking.checkMoveForward(normalized, currentPositionS, 'S'));
+        assertFalse(Checking.checkMoveForward(normalized, currentPositionN, 'N'));
     }
     
     @Test
     public void shouldReturnTrueCheckMoveForward() {
         board = "  N     N     N                                                                                                                                                                                                                                                                   S     S     S  ".toCharArray();
         normalized = Board.armarTablero(board);
-        int[] currentPosition = new int[]{2, 2};
+        int[] currentPositionN = new int[]{2, 2};
+        int[] currentPositionS = new int[]{16, 8};
         
-        assertTrue(Checking.checkMoveForward(normalized, currentPosition, 'N'));
+        assertTrue(Checking.checkMoveForward(normalized, currentPositionN, 'N'));
+        assertTrue(Checking.checkMoveForward(normalized, currentPositionS, 'S'));
+    }
+    
+    @Test
+    public void shouldReturnFalseCheckMoveLeft() {
+        board = "N   N                                                                                                                                               N              -*-              S                                                                                           S       S        ".toCharArray();
+        normalized = Board.armarTablero(board);
+        int[] currentPositionS = new int[]{16, 0};
+        int[] currentPositionN = new int[]{0, 0};
+        
+        assertFalse(Checking.checkMoveLeft(normalized, currentPositionS, 'S'));
+        assertFalse(Checking.checkMoveLeft(normalized, currentPositionN, 'N'));
+    }
+        
+    @Test
+    public void shouldReturnTrueCheckMoveLeft() {
+        board = "  N     N     N                                                                                                                                                                                                                                                                   S     S     S  ".toCharArray();
+        normalized = Board.armarTablero(board);
+        int[] currentPositionN = new int[]{0, 2};
+        int[] currentPositionS = new int[]{16, 8};
+        
+        assertTrue(Checking.checkMoveLeft(normalized, currentPositionN, 'N'));
+        assertTrue(Checking.checkMoveLeft(normalized, currentPositionS, 'S'));
     }
 }
