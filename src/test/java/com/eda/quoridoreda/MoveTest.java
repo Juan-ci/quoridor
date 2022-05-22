f5ac7b57cead799da3f9c21fbbaf9eb85bb76956
Juan Ignacio Caballero <juan.ci.caballero@gmail.com>
2022-05-22 18:56:53 -0300

    Test moveRight succeded.

--- src/test/java/com/eda/quoridoreda/MoveTest.java
+++ src/test/java/com/eda/quoridoreda/MoveTest.java
@@ -35,4 +35,34 @@ public class MoveTest {
         assertArrayEquals(expJmupNResult, resultJumpN);
         assertArrayEquals(expJumpSResult, resulJumpS);
     }
+    
+    @Test
+    public void shouldMoveRight() {
+        board = "    N                                           N                                 S                                                                   N                               S                                                                                                 S        ".toCharArray();
+        normalizedBoard = Board.armarTablero(board);
+        char sideS = 'S';
+        char sideN = 'N';
+        
+        //Single step
+        int[] currentNPosition = {0, 4};
+        int[] currentSPosition = {16, 8};
+        int[] expNResult = {0, 6};
+        int[] expSResult = {16, 10};
+        int[] resultN = Move.moveRight(normalizedBoard, currentNPosition, sideN);
+        int[] resultS = Move.moveRight(normalizedBoard, currentSPosition, sideS);
+        assertArrayEquals(expNResult, resultN);
+        assertArrayEquals(expSResult, resultS);
+    
+        
+        //ADD CHECK AND TEST FOR JUMP TO RIGHT
+        //Jump forward
+//        int[] currentJumpNPosition = {2, 14};
+//        int[] currentJumpSPosition = {4, 14};
+//        int[] expJmupNResult = {6, 14};
+//        int[] expJumpSResult = {0, 14};
+//        int[] resultJumpN = Move.moveRight(normalizedBoard, currentJumpNPosition, sideN);
+//        int[] resulJumpS = Move.moveRight(normalizedBoard, currentJumpSPosition, sideS);
+//        assertArrayEquals(expJmupNResult, resultJumpN);
+//        assertArrayEquals(expJumpSResult, resulJumpS);
+    }
 }
